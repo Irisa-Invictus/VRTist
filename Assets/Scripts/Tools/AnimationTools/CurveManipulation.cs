56e7307e15e71efe0a21d48c8bb06773479ae1b9
MIRABILE Anthony <anthony.mirabile@inria.fr>
2022-09-30 12:34:59 +0200

    curve manipulation

--- Assets/Scripts/Tools/AnimationTools/CurveManipulation.cs
+++ Assets/Scripts/Tools/AnimationTools/CurveManipulation.cs
@@ -30,14 +30,15 @@ namespace VRtist
 
     public class CurveManipulation
     {
-        public bool isHuman;
         public GameObject Target;
         public int Frame;
+        private int startFrame;
+        private int endFrame;
 
         public struct ObjectData
         {
             public AnimationSet Animation;
-            public Matrix4x4 InitialParentMatrix;
+            public Matrix4x4 InitialParentMatrixLocalToWorld;
             public Matrix4x4 InitialParentMatrixWorldToLocal;
             public Matrix4x4 InitialTRS;
             public float ScaleIndice;
@@ -62,37 +63,141 @@ namespace VRtist
 
         private Matrix4x4 initialMouthMatrix;
 
-        private int startFrame;
-        private int endFrame;
         private double continuity;
-        private GameObject _target;
-        private int _frame;
         private Transform _mouthpiece;
         private AnimationTool.PoseEditMode _poseMode;
-        private int _startSelection;
-        private int _endSelection;
 
+        private bool isRig;
 
         public CurveManipulation(GameObject target, int frame, int startSelection, int endSelection, Transform mouthpiece, AnimationTool.PoseEditMode poseMode)
         {
-            _target = target;
-            _frame = frame;
-            _startSelection = startSelection;
-            _endSelection = endSelection;
+            Target = target;
+            Frame = frame;
+            startFrame = startSelection;
+            endFrame = endSelection;
             _mouthpiece = mouthpiece;
             _poseMode = poseMode;
+
+            if (target.TryGetComponent(out JointController jointController))
+            {
+                isRig = true;
+                if (poseMode == AnimationTool.PoseEditMode.FK) RigFKZone(jointController, frame, startSelection, endSelection, mouthpiece);
+                else RigIKZone(jointController, frame, startSelection, endSelection, mouthpiece);
+            }
+            else
+            {
+                isRig = false;
+                ObjectKeyFrameZone(target, frame, startSelection, endSelection, mouthpiece);
+            }
         }
 
         public CurveManipulation(GameObject target, int frame, Transform mouthpiece, AnimationTool.PoseEditMode poseMode)
         {
-            _target = target;
-            _frame = frame;
+            Target = target;
+            Frame = frame;
             _mouthpiece = mouthpiece;
             _poseMode = poseMode;
+
+            if (target.TryGetComponent(out JointController jointController))
+            {
+                isRig = true;
+                if (poseMode == AnimationTool.PoseEditMode.FK) RigFKPoint(jointController, frame, mouthpiece);
+                else RigIKPoint(jointController, frame, mouthpiece);
+            }
+            else
+            {
+                isRig = false;
+                ObjectKeyframePoint(target, frame, mouthpiece);
+            }
         }
+
+        private void ObjectKeyframePoint(GameObject target, int frame, Transform mouthpiece)
+        {
+            AnimationSet previousAnimation = GlobalState.Animation.GetObjectAnimation(target);
+            initialMouthMatrix = mouthpiece.worldToLocalMatrix;
+            startFrame = previousAnimation.GetCurve(AnimatableProperty.RotationX).GetPreviousKey(frame).frame;
+            endFrame = previousAnimation.GetCurve(AnimatableProperty.RotationX).GetNextKey(frame).frame;
+
+            if (!previousAnimation.GetCurve(AnimatableProperty.PositionX).Evaluate(frame, out float posx)) posx = target.transform.localPosition.x;
+            if (!previousAnimation.GetCurve(AnimatableProperty.PositionY).Evaluate(frame, out float posy)) posy = target.transform.localPosition.y;
+            if (!previousAnimation.GetCurve(AnimatableProperty.PositionZ).Evaluate(frame, out float posz)) posz = target.transform.localPosition.z;
+            if (!previousAnimation.GetCurve(AnimatableProperty.RotationX).Evaluate(frame, out float rotx)) rotx = target.transform.localEulerAngles.x;
+            if (!previousAnimation.GetCurve(AnimatableProperty.RotationY).Evaluate(frame, out float roty)) roty = target.transform.localEulerAngles.y;
+            if (!previousAnimation.GetCurve(AnimatableProperty.RotationZ).Evaluate(frame, out float rotz)) rotz = target.transform.localEulerAngles.z;
+            if (!previousAnimation.GetCurve(AnimatableProperty.ScaleX).Evaluate(frame, out float scax)) scax = target.transform.localScale.x;
+            if (!previousAnimation.GetCurve(AnimatableProperty.ScaleY).Evaluate(frame, out float scay)) scay = target.transform.localScale.y;
+            if (!previousAnimation.GetCurve(AnimatableProperty.ScaleZ).Evaluate(frame, out float scaz)) scaz = target.transform.localScale.z;
+
+            Vector3 initialPosition = new Vector3(posx, posy, posz);
+            Quaternion initialRotation = Quaternion.Euler(rotx, roty, rotz);
+            Vector3 initialScale = new Vector3(scax, scay, scaz);
+
+            objectData = new ObjectData()
+            {
+                Animation = new AnimationSet(previousAnimation),
+                InitialParentMatrixLocalToWorld = target.transform.parent.localToWorldMatrix,
+                InitialParentMatrixWorldToLocal = target.transform.parent.worldToLocalMatrix,
+                InitialTRS = Matrix4x4.TRS(initialPosition, initialRotation, initialScale),
+                ScaleIndice = 1,
+            };
+        }
+        private void ObjectKeyFrameZone(GameObject target, int frame, int startFrame, int endFrame, Transform mouthpiece)
+        {
+
+        }
+        private void RigFKPoint(JointController joint, int frame, Transform mouthpiece)
+        {
+
+        }
+        private void RigIKPoint(JointController joint, int frame, Transform mouthpiece)
+        {
+
+        }
+        private void RigFKZone(JointController joint, int frame, int startFrame, int endFrame, Transform mouthpiece)
+        {
+
+        }
+        private void RigIKZone(JointController joint, int frame, int startFrame, int endFrame, Transform mouthpiece)
+        {
+
+        }
+
 
         internal void DragCurve(Transform mouthpiece)
         {
+            //TODO: add scale
+            Matrix4x4 transformation = mouthpiece.localToWorldMatrix * initialMouthMatrix;
+            if (isRig)
+            {
+
+            }
+            else
+            {
+                Matrix4x4 transformed = objectData.InitialParentMatrixWorldToLocal *
+                transformation * objectData.InitialParentMatrixLocalToWorld *
+                objectData.InitialTRS;
+
+                Maths.DecomposeMatrix(transformed, out objectData.lastPosition, out objectData.lastQRotation, out objectData.lastScale);
+                objectData.lastRotation = objectData.lastQRotation.eulerAngles;
+                objectData.lastScale *= 1;
+
+                Interpolation interpolation = GlobalState.Settings.interpolation;
+                AnimationKey posX = new AnimationKey(Frame, objectData.lastPosition.x, interpolation);
+                AnimationKey posY = new AnimationKey(Frame, objectData.lastPosition.y, interpolation);
+                AnimationKey posZ = new AnimationKey(Frame, objectData.lastPosition.z, interpolation);
+                AnimationKey rotX = new AnimationKey(Frame, objectData.lastRotation.x, interpolation);
+                AnimationKey rotY = new AnimationKey(Frame, objectData.lastRotation.y, interpolation);
+                AnimationKey rotZ = new AnimationKey(Frame, objectData.lastRotation.z, interpolation);
+                AnimationKey scalex = new AnimationKey(Frame, objectData.lastScale.z, interpolation);
+                AnimationKey scaley = new AnimationKey(Frame, objectData.lastScale.z, interpolation);
+                AnimationKey scalez = new AnimationKey(Frame, objectData.lastScale.z, interpolation);
+
+
+                AnimationSet ObjectAnimation = GlobalState.Animation.GetObjectAnimation(Target);
+                objectData.Solver = new TangentSimpleSolver(objectData.lastPosition, objectData.lastQRotation, ObjectAnimation, Frame, startFrame, endFrame, 1);
+                objectData.Solver.TrySolver();
+                GlobalState.Animation.onChangeCurve.Invoke(Target, AnimatableProperty.PositionX);
+            }
         }
 
         internal void ReleaseCurve()
